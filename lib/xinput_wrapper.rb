5c84e4d7f6f23aa91d26c9ef7412d92501f0f64f
James Robertson <james@jessie.qbitx.com>
2019-04-07 14:41:19 +0100

    feature: Combination keys can now be captured. e.g. ctrl+g.

--- lib/xinput_wrapper.rb
+++ lib/xinput_wrapper.rb
@@ -11,19 +11,25 @@ class XInputWrapper
   def initialize(device: '3', verbose: true, lookup: {}, debug: false )
 
     # defaults to QWERTY keyboard layout
-    
+    @modifiers = {
+      62 => :shift,     # right control
+      37 => :ctrl,   # left control 
+      50 => :shift,     # left shift
+      64 => :alt,       # alt shift
+      92 => :alt,       # right alt  
+      105 => :ctrl,  # right control     
+      133 => :super,    # left super (windows key)
+      134 => :super     # right super (windows key)
+    }
     @lookup = {
       10=>:"1", 11=>:"2", 12=>:"3", 13=>:"4", 14=>:"5", 15=>:"6", 16=>:"7", 
       17=>:"8", 18=>:"9", 19=>:"0", 20=>:-, 21=>:"=", 22=>:backspace, 
       23=>:tab, 24=>:q, 25=>:w, 26=>:e, 27=>:r, 28=>:t, 29=>:y, 30=>:u, 
-      31=>:i, 32=>:o, 33=>:p, 34=>:"[", 35=>:"]", 36=>nil, 38=>:a, 39=>:s, 
+      31=>:i, 32=>:o, 33=>:p, 34=>:"[", 35=>:"]", 36=>:enter, 38=>:a, 39=>:s, 
       40=>:d, 41=>:f, 42=>:g, 43=>:h, 44=>:j, 45=>:k, 46=>:l, 47=>:";", 
       48=>:"'", 49=>nil, 52=>:z, 53=>:x, 54=>:c, 55=>:v, 56=>:b, 57=>:n, 
-      58=>:m, 59=>:",", 60=>:".", 61=>:/, 62=>:right_shift, 65=>:" ",
+      58=>:m, 59=>:",", 60=>:".", 61=>:/,  65=>:" ",
       9 => :escape,
-      37 => :control,
-      50 => :left_shift,
-      64 => :left_alt,
       66 => :capslock,
       67 => :f1,
       68 => :f2,
@@ -37,10 +43,8 @@ class XInputWrapper
       76 => :f10,
       77 => :numlock,
       78 => :scroll_lock,
-      92 => :right_alt,
       95 => :f11,
       96 => :f12,
-      105 => :control,
       107 => :print_screen,
       110 => :home,
       111 => :up_arrow,
@@ -54,8 +58,6 @@ class XInputWrapper
       122 => :vol_down,
       123 => :vol_up,
       127 => :pause_break,
-      133 => :super,
-      134 => :super,
       135 => :submenu,
       148 => :calc,
       150 => :sleep,
@@ -69,7 +71,7 @@ class XInputWrapper
       174 => :stop,
       179 => :music,
       180 => :browser
-    }.merge(lookup)
+    }.merge(@modifiers).merge(lookup)
     
     @device, @verbose, @debug = device, verbose, debug
     
@@ -80,6 +82,7 @@ class XInputWrapper
     command = "xinput test-xi2 --root #{@device}"
 
     type = 0
+    raw_keys = []
 
     IO.popen(command).each_line do |x|
  
@@ -87,29 +90,52 @@ class XInputWrapper
       raw_type = x[/EVENT type (\d+)/,1]
 
       type = raw_type.to_i unless raw_type.nil?
-      
+      next unless type == 13 or type == 14
+    
+      keycode = x[/detail: (\d+)/,1].to_i
+      next if keycode == 0
+          
       # type = 13 means a key has been pressed
       if type == 13 then
+    
+        raw_keys << keycode
 
-        keycode = x[/detail: (\d+)/,1].to_i
-        next if keycode == 0
+        puts 'raw_keys: ' + raw_keys.inspect if @debug
 
-        puts 'keycode: ' + keycode.to_s if keycode > 0 and @verbose
-        puts ('>keycode: ' + keycode.to_s).debug  if @debug
+        if raw_keys.length <= 1 then
+          puts 'keycode: ' + keycode.to_s if keycode > 0 and @verbose
+          puts ('>keycode: ' + keycode.to_s).debug  if @debug
+      
+          key = @lookup[keycode]
+          
 
-        key = @lookup[keycode]
-        on_key_press(key, keycode)
+
+          puts ('key: ' + key.inspect).debug if @debug
+
+          if key then
+            puts key.to_s + ' key presssed' if @verbose
+            name = "on_#{key}_key".to_sym
+            method(name).call if self.protected_methods.include? name
+            on_key_press key, keycode
+          end        
+    
+        else
+
+          keys = raw_keys.map {|kc| @lookup[kc] }
+          puts ('keys: ' + keys.inspect) if @debug
+          on_key_press(keys.last, keycode, keys[0..-2])    
+
+        end
+    
 
 
-        puts ('key: ' + key.inspect).debug if @debug
+      elsif type == 14
+    
+        index = raw_keys.rindex(keycode)
+        raw_keys.delete_at index if index
 
-        if key then
-          puts key.to_s + ' key presssed' if @verbose
-          name = "on_#{key}_key".to_sym
-          method(name).call if self.protected_methods.include? name
-        end        
-
-      end
+      end    
+    
     end
   end
   
@@ -118,7 +144,14 @@ class XInputWrapper
   def on_control_key()
     puts 'ctrl key pressed'
   end  
-  def on_key_press(key, keycode)  end
+    
+  def on_key_press(key, keycode, modifier)
+    
+    if @debug then
+      puts ('key: ' + key.inspect).debug
+    end
+    
+  end
   
   def on_shift_key()  end
   def on_super_key()  end
@@ -147,3 +180,4 @@ class XInputWrapper
     
 end
 
+# puts  h.sort.map {|x| "%s => :%s" % x}.join(",\n")
